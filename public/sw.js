7ee4d6c7787fcc467f33b37d8eb070ce94462346
orga89265876790-ux <orga89265876790@gmail.com>
2026-08-25 15:52:19 +0300

    Добавить выбор стран и национальностей в Паспорт строителя

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'structos-shell-v38';
+const CACHE_NAME = 'structos-shell-v39';
 const APP_SHELL = ['./', './dashboard.html', './passport.html'];
 
 self.addEventListener('install', (event) => {
