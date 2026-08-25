b0d050f8bc083479daab1ccc9c09901d23ae9a7e
orga89265876790-ux <orga89265876790@gmail.com>
2026-08-25 16:52:44 +0300

    Добавить строительную активность и исправить плавающее меню

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'structos-shell-v41';
+const CACHE_NAME = 'structos-shell-v42';
 const APP_SHELL = ['./', './dashboard.html', './passport.html'];
 
 self.addEventListener('install', (event) => {
