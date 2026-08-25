ebd369fab400c5df00372ed342825333a107b3e9
orga89265876790-ux <orga89265876790@gmail.com>
2026-08-25 15:36:42 +0300

    Сделать меню StructOS плавающим и адаптивным

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'structos-shell-v37';
+const CACHE_NAME = 'structos-shell-v38';
 const APP_SHELL = ['./', './dashboard.html', './passport.html'];
 
 self.addEventListener('install', (event) => {
