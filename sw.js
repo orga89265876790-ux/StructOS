2a301a71259771a1a84728470b45c709319cb058
orga89265876790-ux <orga89265876790@gmail.com>
2026-08-26 18:59:05 +0300

    Добавить каналы StructOS в профиль

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'structos-shell-v46';
+const CACHE_NAME = 'structos-shell-v47';
 const APP_SHELL = ['./', './dashboard.html', './passport.html'];
 
 self.addEventListener('install', (event) => {
