42767eca88488dbe8a5c7b1eef66744e2869e72f
orga89265876790-ux <orga89265876790@gmail.com>
2026-08-27 22:21:33 +0300

    Исправить наложение полей в Паспорте строителя (#45)
    Разделить дату рождения и возраст, а также поля времени «С» и «До» на планшетах.

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'structos-shell-v50';
+const CACHE_NAME = 'structos-shell-v51';
 const APP_SHELL = ['./', './dashboard.html', './passport.html'];
 
 self.addEventListener('install', (event) => {
