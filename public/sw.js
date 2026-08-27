e5eea7b842550dbd8126d5bd975220ef4ccd3cad
orga89265876790-ux <orga89265876790@gmail.com>
2026-08-27 11:03:02 +0300

    Добавить A4-отчёты по коротким договорам

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'structos-shell-v49';
+const CACHE_NAME = 'structos-shell-v50';
 const APP_SHELL = ['./', './dashboard.html', './passport.html'];
 
 self.addEventListener('install', (event) => {
