afe9cae4d35f52c2d048cecc8db04b244c02884d
orga89265876790-ux <orga89265876790@gmail.com>
2026-08-28 18:00:24 +0300

    Исправить наложение выбора сторон документа

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'structos-shell-v57';
+const CACHE_NAME = 'structos-shell-v58';
 const APP_SHELL = ['./', './dashboard.html', './passport.html'];
 
 self.addEventListener('install', (event) => {
