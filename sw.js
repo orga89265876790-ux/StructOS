822aaa3bca8bec8c16af9f55bf942799dc40ab77
orga89265876790-ux <orga89265876790@gmail.com>
2026-08-28 19:40:50 +0300

    Единый выбор версий для редактируемых отчётов (#56)
    После сохранения любой редактируемой версии отчёта пользователь выбирает: удалить предыдущую или оставить обе.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'structos-shell-v61';
+const CACHE_NAME = 'structos-shell-v62';
 const APP_SHELL = ['./', './dashboard.html', './passport.html'];
 
 self.addEventListener('install', (event) => {
