b7bc30551474371321aca04c33fed27ea8f5bc81
orga89265876790-ux <orga89265876790@gmail.com>
2026-08-28 17:30:58 +0300

    Улучшить стороны и историю актов и ведомостей (#50)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'structos-shell-v55';
+const CACHE_NAME = 'structos-shell-v56';
 const APP_SHELL = ['./', './dashboard.html', './passport.html'];
 
 self.addEventListener('install', (event) => {
