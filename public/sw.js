d78e80de0fb81d63cb7c05c023201defa539dfc1
orga89265876790-ux <orga89265876790@gmail.com>
2026-08-29 21:54:53 +0300

    Обновить кэш опубликованного кабинета

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'structos-shell-v76';
+const CACHE_NAME = 'structos-shell-v77';
 const APP_SHELL = ['./', './dashboard.html', './passport.html'];
 
 self.addEventListener('install', (event) => {
