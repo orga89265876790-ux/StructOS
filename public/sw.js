f24c1f3c8d88cb8bd91ce0ab77ca95b2a96962fe
orga89265876790-ux <orga89265876790@gmail.com>
2026-08-29 23:47:22 +0300

    Добавить просмотр и удаление файлов быстрого объекта

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'structos-shell-v78';
+const CACHE_NAME = 'structos-shell-v79';
 const APP_SHELL = ['./', './dashboard.html', './passport.html'];
 
 self.addEventListener('install', (event) => {
