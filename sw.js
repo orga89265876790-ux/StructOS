45c283e51276ebdc15f3c7b7b790eb104bcc9837
orga89265876790-ux <orga89265876790@gmail.com>
2026-09-02 00:00:29 +0300

    Add declarative project workflow menu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'structos-offline-v84';
+const CACHE_NAME = 'structos-offline-v85';
 const CORE_PAGES = ['./', './dashboard.html', './passport.html', './login.html', './manifest.webmanifest'];
 const OPTIONAL_SOURCE_ASSETS = [
   './dashboard.js',
